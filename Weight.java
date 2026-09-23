98185a6eac1108efa40755d26a8cf19811678051
Andy <rothstein.andy@gmail.com>
2026-09-23 12:11:05 -0700

    hghccifif

--- Weight.java
+++ Weight.java
@@ -1,9 +1,42 @@
 public class Weight {
-private int pounds;
-private int ounces;
+    private int pounds;
+    private int ounces;
 
-public void weight (int b){
-    if (0<b<=)
-}
+    public Weight(int pounds, int ounces) {
+        if (ounces <= 0 ){
+            if(ounces<16){
+                this.ounces = ounces;
+            }
+
+        }
+        if(pounds>0)
+            this.pounds= pounds;
+            
+    }
+
+    public Weight(int ounces){
+        this(0,ounces);
+
+    }
+    public int totalOunces(){
+         return this.ounces + 16 * this.pounds;
+        }
     
+    public boolean isHeavier( Weight other){
+        int totalW = totalOunces();
+        int otherW = other.totalOunces();
+        return totalW > otherW;
+
+    }
+    public Weight multiple(int scale){
+        int newweight = totalOunces() *scale;
+        int newpounds = newweight / 16;
+        int newounces = totalOunces() - newpounds * 16;
+        return new Weight(newpounds,newounces);
+    }
+    public void print(){
+        System.out.println(this.pounds + "pounds" + this.ounces + "ounces");
+    }
+
 }
+
--- WeightMain.java
+++ WeightMain.java
@@ -1,3 +1,13 @@
 public class WeightMain {
+    public static void main(String[] args) {
+        
     
+   Weight w =new Weight(5,15);
+   Weight w1= new Weight(13);
+   boolean b = w.isHeavier(w1);
+   System.out.println(b);
+
+   Weight w2 =w.multiple(2);
+   w2.print();
+}
 }
